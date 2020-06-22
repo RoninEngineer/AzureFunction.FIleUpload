7400cb466f22cb15d3bd5ff9eca4626f3c546292
James Walsh Jr <walshjrjames@gmail.com>
2020-06-21 21:42:52 -0400

    Added multipart request validation Engine and repository methods

--- AzureFunctions.Engine/Engine/FileUploadEngine.cs
+++ AzureFunctions.Engine/Engine/FileUploadEngine.cs
@@ -1,11 +1,31 @@
-﻿using AzureFunction.Engine.Interface;
+﻿using AzureFunction.Engine.Attributes;
+using AzureFunction.Engine.Interface;
+using Microsoft.AspNetCore.Http;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace AzureFunction.Engine.Engine
 {
     public class FileUploadEngine : IFileUploadEngine
     {
+        public async Task<bool> IsMultipartContent(HttpRequest request)
+        {
+            try
+            {
+                var isMultipart = await Task.Run(() => MultipartRequest.IsMultipartContentType(request.ContentType));
+                if (!isMultipart)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
